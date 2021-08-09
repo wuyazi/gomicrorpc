955568977c163564034b85b381352bed9f5a5203
xiandong <xiandong@italki.com>
2021-08-09 14:59:30 +0800

    example1 runuable

--- example1/cli/main.go
+++ example1/cli/main.go
@@ -3,38 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	_ "github.com/asim/go-micro/plugins/registry/consul/v3"
+	"github.com/asim/go-micro/v3"
 	"github.com/lpxxn/gomicrorpc/example1/proto"
-	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/registry"
-	"github.com/micro/go-plugins/registry/etcdv3"
 )
 
 func main() {
-	// 我这里用的etcd 做为服务发现
-	reg := etcdv3.NewRegistry(func(op *registry.Options) {
-		op.Addrs = []string{
-			"http://192.168.3.34:2379", "http://192.168.3.18:2379", "http://192.168.3.110:2379",
-		}
-	})
 
 	// 初始化服务
-	service := micro.NewService(
-		micro.Registry(reg),
-	)
+	service := micro.NewService()
 
-	// 2019年源码有变动默认使用的是mdns面不是consul了
-	// 如果你用的是默认的注册方式把上面的注释掉用下面的
-	/*
-		// 初始化服务
-		service := micro.NewService(
-			micro.Name("lp.srv.eg1"),
-		)
-	*/
 	service.Init()
 
-	sayClent := model.NewSayService("lp.srv.eg1", service.Client())
+	sayClent := proto.NewSayService("lp.srv.eg1", service.Client())
 
-	rsp, err := sayClent.Hello(context.Background(), &model.SayParam{Msg: "hello server"})
+	rsp, err := sayClent.Hello(context.Background(), &proto.SayParam{Msg: "hello server"})
 	if err != nil {
 		panic(err)
 	}
--- example1/srv/main.go
+++ example1/srv/main.go
@@ -3,52 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+	_ "github.com/asim/go-micro/plugins/registry/consul/v3"
+	"github.com/asim/go-micro/v3"
 	"github.com/lpxxn/gomicrorpc/example1/proto"
-	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/registry"
-	"github.com/micro/go-plugins/registry/etcdv3"
 )
 
 type Say struct{}
 
-func (s *Say) Hello(ctx context.Context, req *model.SayParam, rsp *model.SayResponse) error {
+func (s *Say) Hello(ctx context.Context, req *proto.SayParam, rsp *proto.SayResponse) error {
 	fmt.Println("received", req.Msg)
-	rsp.Header = make(map[string]*model.Pair)
-	rsp.Header["name"] = &model.Pair{Key: 1, Values: "abc"}
+	rsp.Header = make(map[string]*proto.Pair)
+	rsp.Header["name"] = &proto.Pair{Key: 1, Values: "abc"}
 
 	rsp.Msg = "hello world"
 	rsp.Values = append(rsp.Values, "a", "b")
-	rsp.Type = model.RespType_DESCEND
+	rsp.Type = proto.RespType_DESCEND
 
 	return nil
 }
 
 func main() {
-	// 我这里用的etcd 做为服务发现
-	reg := etcdv3.NewRegistry(func(op *registry.Options) {
-		op.Addrs = []string{
-			"http://192.168.3.34:2379", "http://192.168.3.18:2379", "http://192.168.3.110:2379",
-		}
-	})
 
 	// 初始化服务
 	service := micro.NewService(
 		micro.Name("lp.srv.eg1"),
-		micro.Registry(reg),
 	)
 
-	// 2019年源码有变动默认使用的是mdns面不是consul了
-	// 如果你用的是默认的注册方式把上面的注释掉用下面的
-	/*
-		// 初始化服务
-		service := micro.NewService(
-			micro.Name("lp.srv.eg1"),
-		)
-	*/
 	service.Init()
 
 	// 注册 Handler
-	model.RegisterSayHandler(service.Server(), new(Say))
+	proto.RegisterSayHandler(service.Server(), new(Say))
 
 	// run server
 	if err := service.Run(); err != nil {
